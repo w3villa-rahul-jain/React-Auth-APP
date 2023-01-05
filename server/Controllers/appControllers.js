bc9927e3bb723d0dc300c89e037701d70abd0c03
Rahul jain <rahul.jain@w3villa.com>
2023-01-05 15:32:32 +0530

    update user function done

--- server/Controllers/appControllers.js
+++ server/Controllers/appControllers.js
@@ -148,7 +148,25 @@ export async function getUser(req, res) {
 }
 
 export async function updateUser(req, res) {
-  res.json("updateUser route");
+  try{
+    const id = req.query.id;
+
+    if(id){
+      const body = req.body;
+      UserModel.updateOne({_id: id}, body, function(err, data){
+        if(err) throw err;
+        
+        return res.status(201).send({msg: "Record Updataed successfully"})
+      })
+
+    }else{
+      return res.status(401).send({error: "Invalid User"});
+    }
+
+
+  }catch(error){
+    return res.send(401).send({error});
+  }
 }
 
 export async function generateOTP(req, res) {
