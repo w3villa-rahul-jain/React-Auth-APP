199e1bdb2d9e1fb2eacbd7b66589046eafab2fe7
Rahul jain <rahul.jain@w3villa.com>
2023-01-05 11:41:39 +0530

    create controller

--- server/router/route.js
+++ server/router/route.js
@@ -2,22 +2,25 @@ import { Router } from 'express';
 
 const router = Router();
 
+// import all controllers
+import * as controller from '../Controllers/appControllers.js'
+
 
 router.route('/register').post((req, res)=> res.json('register Route'));
 router.route('/registerMail').post();  //send the email
-router.route('/authentication').post(); //authenticate user
-router.route('/login').post();  //logini in app
+router.route('/authentication').post((req, res)=> res.end()); //authenticate user
+router.route('/login').post(controller.login);  //logini in app
 
 // get method
-router.route('/user/:username').get(); //user with username
-router.route('/generateOTP').get();  //generate random OTP
-router.route('/verifyOTP').get();  //verify generated OTP
-router.route('createResetSession').get(); //reset all the variables
+router.route('/user/:username').get(controller.getUser); //user with username
+router.route('/generateOTP').get(controller.generateOTP);  //generate random OTP
+router.route('/verifyOTP').get(controller.verifyOTP);  //verify generated OTP
+router.route('createResetSession').get(controller.createResetSession); //reset all the variables
 
 // put methods
   
-router.route('/updateUser').put();  //is use to update the user profile
-router.route('/resetPassword').put();  // use to reset password
+router.route('/updateUser').put(controller.updateUser);  //is use to update the user profile
+router.route('/resetPassword').put(controller.resetPassword);  // use to reset password
 
 
 
